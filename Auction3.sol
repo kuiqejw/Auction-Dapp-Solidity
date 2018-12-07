5e6490f3591452df18c147ae55931dc31f28daa9
kuiqejw <lauraong1@yahoo.com.sg>
2018-12-08 07:59:17 +0800

    Update Auction3.sol

--- Auction3.sol
+++ Auction3.sol
@@ -17,7 +17,7 @@ contract AuctionHouse{
         mapping(address => uint) pendingWithdrawal;
         //Allowed withdrawals of previous bids
         //cancelled?
-        //bool public cancelled;
+        bool cancelled;
     }
     
    
@@ -32,9 +32,9 @@ contract AuctionHouse{
         time = now;
         owner = msg.sender;
     }
-    Item i;
    // our setter in this case
     function create(string _n) public returns(uint) {
+        Item memory i;
         i.itemDesc = _n;
         i.itemOwner = msg.sender;
         //Item[] storage _l = list[msg.sender];
@@ -56,17 +56,19 @@ contract AuctionHouse{
         address maxBidder = idList[ix].maxBidder;
         return maxBidder;
     }
+    function getOwner(uint ix) public view returns(address){
+        return idList[ix].itemOwner;
+    }
     function bid(uint uniq_id) public payable returns(uint){
         require(msg.value>= 0);
         require(msg.sender != idList[uniq_id].itemOwner);
         //if all the requirements above are satisfied then we invoke this
-
+        Item storage i;
         //total bid = current amount that they've sent to the contract plus whatever has been sent with this transaction
         i = idList[uniq_id];
         uint newBid = msg.value;
         require(newBid > i.maxBid, "Sorry, your bid value is too little!");
         if(msg.sender != i.maxBidder){//why are you paying more?
-            i.pendingWithdrawal[msg.sender] = newBid;
             i.maxBidder = msg.sender;
             i.maxBid = newBid;
         }
@@ -75,14 +77,40 @@ contract AuctionHouse{
         return newBid;
     }
     //returns cash upon successful bid; based off withdrawal design patterns in readthedocs
-    function withdraw(uint uniq_id) public returns(bool){
-        uint amount  = idList[uniq_id].pendingWithdrawal[msg.sender];
-        idList[uniq_id].pendingWithdrawal[msg.sender] = 0;
+    function withdraw(uint uniq_id) public returns(string){
+        Item storage i;
+        i = idList[uniq_id];
+        uint amount  = i.pendingWithdrawal[msg.sender];
+        i.pendingWithdrawal[msg.sender] = 0;
         if (!msg.sender.send(amount)){
-            idList[uniq_id].pendingWithdrawal[msg.sender] = amount;
-            return false;
+            i.pendingWithdrawal[msg.sender] = amount;
+            return "false";
         }
-        return true;
+        idList[uniq_id] = i;
+        return "true";
+        
+    }
+    function endAuction(uint uniq_id) public {
+        Item memory i;
+        i = idList[uniq_id];
+        require(msg.sender == idList[uniq_id].itemOwner,"You cannot end the auction if you are not the owner");
+        require(!i.cancelled, "This auction has been cancelled with penalty. You can't end it!");
+        require(!i.ended, "this auction has already ended. ");
+        i = idList[uniq_id];
+        i.ended = true;
+        msg.sender.transfer(i.maxBid);
+    }
+    function cancelAuction (uint uniq_id) public payable{
+        //you need to pay a penalty if you want to cancel before the auction ends!
+        Item memory i;
+        i = idList[uniq_id];
+        require(msg.sender == idList[uniq_id].itemOwner,"You cannot end the auction if you are not the owner");
+        require(!i.cancelled, "This auction has been cancelled with penalty. You can't end it!");
+        require(!i.ended, "this auction has already ended. ");
+        i = idList[uniq_id];
+        i.ended = true;
+        i.maxBidder.transfer(i.maxBid);
+        
         
     }
 
